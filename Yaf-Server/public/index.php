74e5c917c1de46cef4feba872e1d2a5be1f76185
fingerQin <753814253@qq.com>
2019-01-17 16:39:34 +0800

    1) 去掉 error_reporting()。

--- Yaf-Server/public/index.php
+++ Yaf-Server/public/index.php
@@ -9,6 +9,5 @@ define('MICROTIME', microtime());
 define('APP_PATH', dirname(dirname(__FILE__)));
 require(APP_PATH . '/vendor/autoload.php');
 require(APP_PATH . '/config/constants.php');
-error_reporting(0);
 $app = new \Yaf_Application(APP_PATH . '/config/config.ini', 'conf');
 $app->bootstrap()->run();
