f3d234459242906d0090b8a5f1c6db9253a10796
fingerQin <753814253@qq.com>
2019-07-13 10:44:12 +0800

    1) edit!

--- Yaf-Server/apps/modules/Cli/controllers/Index.php
+++ Yaf-Server/apps/modules/Cli/controllers/Index.php
@@ -6,6 +6,8 @@
  */
 
 use Utils\YExcel;
+use Models\Event;
+use Services\Event\Producer;
 
 class IndexController extends \Common\controllers\Cli
 {
@@ -19,5 +21,25 @@ class IndexController extends \Common\controllers\Cli
             ['张三', '男', '28']
         ];
         YExcel::createExcel($headerTitle, $data, __DIR__, 'tests');
+    }
+
+    /**
+     * 登录事件数据 Push。
+     */
+    public function threadPushAction()
+    {
+        $datetime  = date('Y-m-d H:i:s', time());
+        for ($i = 0; $i < 1000; $i++) {
+            Producer::push([
+                'code'        => Event::CODE_LOGIN,
+                'userid'      => 1,
+                'mobile'      => '18575202691',
+                'platform'    => 1,
+                'app_v'       => '0.0.1',
+                'v'           => '1.0.0',
+                'login_time'  => $datetime
+            ]);
+        }
+        echo "ok:{$datetime}\n";
     }
 }
